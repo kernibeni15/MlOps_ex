c941c37001e48f02d63af96420a7e169399c4af4
kernibeni15 <kernibeni17@gmail.com>
2024-01-07 19:48:33 +0100

    upgrade

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn, Tensor
+
+# src/models/model.py
+def forward(self, x: Tensor):
+    if x.ndim != 4:
+        raise ValueError('Expected input to a 4D tensor')
+    if x.shape[1] != 1 or x.shape[2] != 28 or x.shape[3] != 28:
+        raise ValueError('Expected each sample to have shape [1, 28, 28]')
+
+myawesomemodel = nn.Sequential(
+    nn.Conv2d(1, 32, 3),  # [B, 1, 28, 28] -> [B, 32, 26, 26]
+    nn.LeakyReLU(),
+    nn.Conv2d(32, 64, 3), # [B, 32, 26, 26] -> [B, 64, 24, 24]
+    nn.LeakyReLU(),
+    nn.MaxPool2d(2),      # [B, 64, 24, 24] -> [B, 64, 12, 12]
+    nn.Flatten(),        # [B, 64, 12, 12] -> [B, 64 * 12 * 12]
+    nn.Linear(64 * 12 * 12, 10),
+)
